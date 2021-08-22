4b0bb100fd4d538a3f2b8a1459942a3add2df7e6
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2021-08-22 09:09:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,59 @@ Webcam.set({
         console.log("model loaded");
     }
     
+    prediction_one="";
+ 
+    
+    function speak(){
+    var api= window.speechSynthesis;
+    speak_data="The hand gesture you showed means"+prediction_one;
+    converted_data= new SpeechSynthesisUtterance(speak_data);
+    api.speak(converted_data);
+    }
+    
+    function predict_Gesture(){
+        img = document.getElementById("captured_image");
+        classifier.classify(img, got_result);
+    }
+    
+    
+    function got_result(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+    
+        if(results[0].label== "Everything's OK"){
+            document.getElementById("emoji_1").innerHTML= "&#128076";
+            prediction_one="Everythings Ok";
 
+        } 
+        if(results[0].label== "Victory"){
+            document.getElementById("emoji_1").innerHTML= "&#9996";
+            prediction_one="Victory";
+
+        } 
+        if(results[0].label== "Crossed Fingers"){
+            document.getElementById("emoji_1").innerHTML= "&#129310";
+            prediction_one="Hope For The Best";
+
+        } 
+        if(results[0].label== "Thumbs Up"){
+            document.getElementById("emoji_1").innerHTML= "&#128077";
+            prediction_one="Great Job";
+
+        } 
+        if(results[0].label== "Thumbs Down"){
+            document.getElementById("emoji_1").innerHTML= "&#128078";
+            prediction_one="Bad Job";
+
+        } 
+        speak();
+}
+
+    
+    
+    }
+    
     
     
